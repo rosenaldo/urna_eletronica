913f3922514825ce34884512ca6bc03719b69ad8
Jose Rosenaldo da S Junior <joserdasjunior@gmail.com>
2022-12-12 12:28:15 -0300

    Implementação dos parametros e botões

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="urna">
+<teclado/>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import '@/css/global.css';
+import teclado from '@/components/urna-teclado.vue'
+
+export default {
+  name: 'App',
+  components: {
+    teclado,
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: var(--background-color);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.urna{
+  width: 1000px;
+  height: 500px;
+  background-color: var(--ballot-box-background-color);
+  padding: 30px;
+  border-radius: 5px;
+  display: flex;
+}
+</style>
---
+++ src/components/urna-teclado.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="urna-teclado">
+        <div class="urna-teclado-numerico">
+            <div class="urna-teclado-numerico-linha">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            </div>
+            <div class="urna-teclado-numerico-linha">
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+            </div>
+            <div class="urna-teclado-numerico-linha">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+            </div>
+            <div class="urna-teclado-numerico-linha">
+                <button>0</button>
+            </div>
+        </div>
+        <div class="urna-teclado-acoes">
+<button class="btn-branco">BRANCO</button>
+<button class="btn-corrige">CORRIGE</button>
+<button class="btn-confirma">CONFIRMA</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'urna_teclado'
+}
+</script>
+<style>
+.urna-teclado{
+    width: 40%;
+    height: 100%;
+    background-color: var(--ballot-box-keyboard-color);
+    border-radius: 5px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+.urna-teclado-numerico{
+    width: 100%;
+}
+.urna-teclado-numerico-linha{
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 20px;
+}
+
+.urna-teclado-numerico button{
+    background-color: var(--ballot-box-keyboard-button-color);
+    color: var(--light-text-color);
+    font-size: 30px;
+    border-radius: 5px;
+    width: 80px;
+    height: 50px;
+}
+
+.urna-teclado-acoes{
+    widows: 100%;
+    display: flex;
+    justify-content: space-around;
+}
+.urna-teclado-acoes button{
+    color: var(--dark-text-color);
+    font-size: 15px;
+    border-radius: 5px;
+    width: 30px;
+    height: 50px;
+}
+.btn-branco{
+    background-color: var(--ballot-box-white-button-color);
+}
+.btn-corrige{
+    background-color: var(--ballot-box-correct-button-color);
+}
+.btn-confirma{
+    background-color: var(--ballot-box-confirm-button-color);
+}
+
+</style>
